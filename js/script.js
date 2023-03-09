2aa0d23034ee3e4d1eef22878e6ed4f1e4ef289d
Angelop26 <angeloprospero26@gmail.com>
2023-03-09 17:16:42 +0100

    project complete

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 // Attivazione del bottone
 
-const ticketGenerator = document.getElementById('tcktgenerator');
+const ticketGenerator = document.getElementById('ms_tcktgenerator');
 ticketGenerator.addEventListener('click', function(){
     // INPUT
     // prendere valori di input
-    const userDataInput = document.getElementById('userName');
+    const userDataInput = document.getElementById('ms_userName');
     userData = userDataInput.value;
 
-    const userKmInput = document.getElementById('user-km');
+    const userKmInput = document.getElementById('ms_user-km');
     userKmString = userKmInput.value;
     userKm = parseFloat (userKmString);
 
-    const userAgeInput = document.getElementById('userAge');
+    const userAgeInput = document.getElementById('ms_userAge');
     userAge = userAgeInput.value;
 
     // ripulire la pagina
@@ -21,28 +21,47 @@ ticketGenerator.addEventListener('click', function(){
 
     // LOGICA
     const basePrice = userKm * 0.21
-    let message = ''
+    let tcktDiscount = 'biglietto standard';
+    let price = ``;
     if (!isNaN(userKm)){
         
         if(userAge === "underage") {
             const TicketPrice20 = (basePrice * 0.8).toFixed(2)
-            message = `Il prezzo del biglietto è ${TicketPrice20}`
-        } else if(userAge === "over 65") {
+            tcktDiscount = 'biglietto junior'
+            price =`${TicketPrice20}€`
+        } else if(userAge === "over") {
             const TicketPrice40 = (basePrice * 0.6).toFixed(2)
-            message = `Il prezzo del biglietto è ${TicketPrice40}`
+            tcktDiscount = 'biglietto senior'
+            price = `${TicketPrice40}€`
         } else {
-            message = `Il prezzo del biglietto è ${basePrice.toFixed(2)}€`
+            price = `${basePrice}€`
         }
    
+        const nmbCab = Math.floor(Math.random() * 10) + 1;
+        const CPCode = Math.floor(Math.random() * 100000) + 1;
+        
+        // OUTPUT
+        document.getElementById('userName').innerHTML = userData
+        document.getElementById('tckt-discount').innerHTML = tcktDiscount
+        document.getElementById('number-cab').innerHTML = nmbCab
+        document.getElementById('CP-code').innerHTML = CPCode
+        document.getElementById('price').innerHTML = price
     } else {
-        message = `immetere i dati nel modo corretto`
+        alert('Immetere i dati nel modo corretto')
     }
+});
 
-    console.log(message);
-
-
+const cancel = document.getElementById('ms_cancel');
+cancel.addEventListener('click', function(){
+    userData =''
+    nmbCab =''
+    CPCode = ''
+    price = ''
 
     // OUTPUT
-    const result = document.getElementById('result')
-    result.innerHTML = message
-});
+    document.getElementById('userName').innerHTML = ''
+    document.getElementById('tckt-discount').innerHTML = ''
+    document.getElementById('number-cab').innerHTML = ''
+    document.getElementById('CP-code').innerHTML = ''
+    document.getElementById('price').innerHTML = ''
+} )
